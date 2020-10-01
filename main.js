ff77d6fd72152c0a836db1cb0e7a96920d63e38d
fernando duenas <fernando39615@aim.com>
2020-10-01 14:12:53 -0500

    last commit

--- main.js
+++ main.js
@@ -7,14 +7,11 @@ const isValidSecondary = require('./is-valid-secondary.js')
 // Your code here!
 const color1 = process.argv[2]
 const color2 = process.argv[3]
-const colorDeconstructor1 = colorDeconstructor(color);
 
 if (color1 === undefined && color2 === undefined) {
     console.log('We need at least one color');
-}
-if (color1 !== undefined && color2 === undefined) {
-    console.log('you enter ' + color1)
-}
-if (color1 !== undefined && color2 !== undefined) {
-    console.log('Color1 and color2' + colorDeconstructor1)
+} else if (color2 === undefined) {
+    console.log('that color deconstructed makes ' + colorDeconstructor(color1))
+} else {
+    console.log('those colors combined make ' + colorCombinator(color1, color2))
 }
